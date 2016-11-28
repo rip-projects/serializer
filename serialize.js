bc0c75d827913b9d8c14435599d6802e52e55437
Ganesha <reekoheek@gmail.com>
2016-11-28 22:01:22 +0700

    initial commit

---
+++ serialize.js
@@ -0,0 +1,25 @@
+function serialize (value) {
+  switch (typeof value) {
+    case 'boolean':
+      return value ? '' : undefined;
+
+    case 'object':
+      if (value instanceof Date) {
+        return value;
+      } else if (value instanceof RegExp) {
+        return value.toString().slice(1, -1);
+      } else if (value) {
+        try {
+          return JSON.stringify(value);
+        } catch (err) {
+          return '';
+        }
+      }
+      break;
+    default:
+      // noop
+  }
+  return value === null ? undefined : value;
+}
+
+export default serialize;
